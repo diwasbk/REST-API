2af98664aec6e9a0aaaa236e3717f9e0a114c79d
diwasbk <diwasbk903@gmail.com>
2025-02-03 00:33:57 +0545

    GET

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+//Routes
+app.use('/products', productRoute);
 
 // Start server
 const PORT = process.env.PORT || 5000;
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -2,5 +2,24 @@ const express = require("express");
 const Product = require("../models/productModel");
 
 const router = express.Router();
-
+// Get all products
+router.get('/', async (req, res) => {
+    try {
+      const products = await Product.findAll();
+      res.json(products);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  });
+// Get a single product by ID
+router.get('/:id', async (req, res) => {
+    try {
+      const { id } = req.params;
+      const product = await Product.findOne({where:{id}});
+      res.json(product);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  });
+  
 module.exports = router
